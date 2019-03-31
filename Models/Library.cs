776a2184059abcced7171f5c015fae5c6184de9a
Caleb Sherfick <calebsherfick@gmail.com>
2019-03-30 21:46:47 -0600

    finished up library. fully works!

--- Models/Library.cs
+++ Models/Library.cs
@@ -9,22 +9,84 @@ namespace console_library.Models
     public string Name { get; set; }
     private List<Book> Books { get; set; }
 
+    private List<Book> CheckedOut { get; set; }
 
-    public void
 
-    public void PrintBooks()
-    {
-      for (int i = 0; i < Books.Count; i++)
-      {
-        Console.WriteLine($"{i + 1} {Books[i].Title} - {Books[i].Author}");
-      }
-    }
     public Library(string location, string name)
     {
       Location = location;
       Name = name;
       Books = new List<Book>();
+      CheckedOut = new List<Book>();
     }
 
+    public void PrintBooks()
+    {
+      System.Console.WriteLine("Please choose from the available books:");
+      for (int i = 0; i < Books.Count; i++)
+      {
+        Book currentBook = Books[i];
+        Console.WriteLine($"{i + 1} {currentBook.Title} - {currentBook.Author}");
+      }
+      System.Console.WriteLine("Seleect a book number to checkout (Q)uit, or (R)eturn a book");
+    }
+
+    public void PrintCheckedOut()
+    {
+      System.Console.WriteLine("Books you have hcecked out.");
+      for (int i = 0; i < CheckedOut.Count; i++)
+      {
+        Book currentBook = CheckedOut[i];
+        Console.WriteLine($"{i + 1} {currentBook.Title} - {currentBook.Author}");
+      }
+      System.Console.WriteLine("Seleect a book number to return, (Q)uit, or (A)vailable to see available books");
+    }
+
+    public void AddBook(Book book)
+    {
+      Books.Add(book);
+    }
+
+
+    public void Checkout(string input)
+    {
+      Book selectedBook = ValidateBook(input, Books);
+      if (selectedBook == null)
+      {
+        Console.Clear();
+        Console.WriteLine("INVALID SELEECTION");
+      }
+      selectedBook.Available = false;
+      CheckedOut.Add(selectedBook);
+      Books.Remove(selectedBook);
+      Console.Clear();
+      System.Console.WriteLine("Don't forget to return it when you're done!");
+    }
+
+
+    public void Return(string input)
+    {
+      Book selectedBook = ValidateBook(input, CheckedOut);
+      if (selectedBook == null)
+      {
+        Console.Clear();
+        Console.WriteLine("INVALID SELEECTION");
+      }
+      selectedBook.Available = true;
+      Books.Add(selectedBook);
+      CheckedOut.Remove(selectedBook);
+      Console.Clear();
+      System.Console.WriteLine("Thank you for returning the book this time.");
+    }
+
+    private Book ValidateBook(string input, List<Book> booksList)
+    {
+      int bookIndex;
+      if (Int32.TryParse(input, out bookIndex) && bookIndex > 0 && bookIndex <= booksList.Count)
+      {
+        return booksList[bookIndex - 1];
+      }
+      return null;
+    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -7,9 +7,69 @@ namespace console_library
   {
     static void Main(string[] args)
     {
+      Console.Clear();
+      //initialize library
+      Library myLibrary = new Library("3742 Library St.", "Caleb's Library");
 
-      Book whereTheSidewalkEnds = new Book("Where the Sidewalk Ends", "Shel Silverstein");
+      //initialize books
+      Book WTSW = new Book("Where the Sidewalk Ends", "Shel Silverstein");
+      Book Cat = new Book("Cat in the Hat", "Dr. Seuss");
+      Book Ring = new Book("Lord of the Rings", "J.R. Tolkin");
+      myLibrary.AddBook(WTSW);
+      myLibrary.AddBook(Cat);
+      myLibrary.AddBook(Ring);
+      System.Console.WriteLine("Welcome to the Library!");
 
+      Enum activeMenu = Menus.CheckoutBook;
+      bool inLibrary = true;
+      while (inLibrary)
+      {
+        switch (activeMenu)
+        {
+          case Menus.CheckoutBook:
+            myLibrary.PrintBooks();
+            break;
+          case Menus.ReturnBook:
+            myLibrary.PrintCheckedOut();
+            break;
+        }
+
+        string input = Console.ReadLine();
+        switch (input.ToUpper()[0])
+        {
+          case 'Q':
+            inLibrary = false;
+            break;
+          case 'R':
+            activeMenu = Menus.ReturnBook;
+            Console.Clear();
+            break;
+          case 'A':
+            activeMenu = Menus.CheckoutBook;
+            Console.Clear();
+            break;
+          default:
+            if (activeMenu.Equals(Menus.CheckoutBook))
+            {
+              myLibrary.Checkout(input);
+            }
+            else
+            {
+              myLibrary.Return(input);
+            }
+            break;
+
+        }
+
+      }
+      System.Console.WriteLine("Have a great day!");
+    }
+
+
+    public enum Menus
+    {
+      CheckoutBook,
+      ReturnBook
     }
   }
 }
