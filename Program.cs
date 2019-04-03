8da2227d9f841689fc7a321d44242d19160b719e
Caleb Sherfick <calebsherfick@gmail.com>
2019-04-03 00:28:38 -0600

    tried 2 refaaddtor & + complex at same time =broke

--- Program.cs
+++ Program.cs
@@ -15,50 +15,148 @@ namespace console_library
       Book WTSW = new Book("Where the Sidewalk Ends", "Shel Silverstein");
       Book Cat = new Book("Cat in the Hat", "Dr. Seuss");
       Book Ring = new Book("Lord of the Rings", "J.R. Tolkin");
+      Book Harry = new Book("Harry Potter", "JK Rowling");
+      Newspaper NYT = new Newspaper("The New York Times", "April 2, 2019");
+      Newspaper IS = new Newspaper("The Idaho Statesman", "April 2, 2019");
+      Newspaper WP = new Newspaper("The Washington Post", "April 2, 2019");
+      Magazine Time = new Magazine("TIME", "March 24, 2019");
+      Magazine People = new Magazine("PEOPLE", "March 16, 2019");
+      Magazine Highlights = new Magazine("Highlights", "March 29, 2019");
+
+
       myLibrary.AddBook(WTSW);
       myLibrary.AddBook(Cat);
       myLibrary.AddBook(Ring);
       System.Console.WriteLine("Welcome to the Library!");
 
-      Enum activeMenu = Menus.CheckoutBook;
+      Enum activeMenu = Menus.Welcome;
       bool inLibrary = true;
       while (inLibrary)
       {
         switch (activeMenu)
         {
+          case Menus.Welcome:
+            System.Console.WriteLine(@"Would you like to view (P)ublications, (V)ideos, (S)ports Equipment
+                          OR
+(R)eturn an item?");
+            WelcomeCommands();
+            break;
+          case Menus.Publication:
+            myLibrary.PrintPublications();
+            PublicationCommands();
+            break;
+          case Menus.Newspaper:
+            System.Console.WriteLine("Here are the Newspapers and Magazines we have on hand. Feel free to read whatever you like.");
+            myLibrary.PrintPapers();
+            myLibrary.PrintMagazines();
+
+            PublicationCommands();
+            break;
+          // case Menus.Sport:
+          //   myLibrary.PrintBooks();
+          //   break;
+          // case Menus.Video:
+          //   myLibrary.PrintBooks();
+          //   break;
           case Menus.CheckoutBook:
             myLibrary.PrintBooks();
+            BookCommands();
             break;
           case Menus.ReturnBook:
             myLibrary.PrintCheckedOut();
+            BookCommands();
             break;
         }
 
-        string input = Console.ReadLine();
-        switch (input.ToUpper()[0])
+        void WelcomeCommands()
         {
-          case 'Q':
-            inLibrary = false;
-            break;
-          case 'R':
-            activeMenu = Menus.ReturnBook;
-            Console.Clear();
-            break;
-          case 'A':
-            activeMenu = Menus.CheckoutBook;
-            Console.Clear();
-            break;
-          default:
-            if (activeMenu.Equals(Menus.CheckoutBook))
-            {
-              myLibrary.Checkout(input);
-            }
-            else
-            {
-              myLibrary.Return(input);
-            }
-            break;
+          string input = Console.ReadLine();
+          switch (input.ToUpper()[0])
+          {
+            case 'P':
+              activeMenu = Menus.Publication;
+              Console.Clear();
+              break;
+            case 'V':
+              activeMenu = Menus.ReturnBook;
+              Console.Clear();
+              return;
+            case 'S':
+              activeMenu = Menus.CheckoutBook;
+              Console.Clear();
+              return;
+            default:
+              if (activeMenu.Equals(Menus.CheckoutBook))
+              {
+                myLibrary.Checkout(input);
+              }
+              else
+              {
+                myLibrary.Return(input);
+              }
+              break;
+          }
+        }
 
+        void PublicationCommands()
+        {
+          string input = Console.ReadLine();
+          switch (input.ToUpper()[0])
+          {
+            case 'N':
+              activeMenu = Menus.Newspaper;
+              Console.Clear();
+              break;
+            case 'M':
+              activeMenu = Menus.ReturnBook;
+              Console.Clear();
+              return;
+            case 'B':
+              activeMenu = Menus.CheckoutBook;
+              Console.Clear();
+              return;
+            default:
+              if (activeMenu.Equals(Menus.CheckoutBook))
+              {
+                myLibrary.Checkout(input);
+              }
+              else
+              {
+                myLibrary.Return(input);
+              }
+              break;
+
+          }
+        }
+
+        void BookCommands()
+        {
+          string input = Console.ReadLine();
+          switch (input.ToUpper()[0])
+          {
+            case 'Q':
+              inLibrary = false;
+              break;
+            case 'R':
+              activeMenu = Menus.ReturnBook;
+              Console.Clear();
+              return;
+            case 'A':
+              activeMenu = Menus.CheckoutBook;
+              Console.Clear();
+              return;
+            default:
+              if (activeMenu.Equals(Menus.CheckoutBook))
+              {
+                myLibrary.Checkout(input);
+              }
+              else
+              {
+                myLibrary.Return(input);
+              }
+              break;
+
+          }
         }
 
       }
@@ -68,8 +166,17 @@ namespace console_library
 
     public enum Menus
     {
+      Welcome,
       CheckoutBook,
-      ReturnBook
+      ReturnBook,
+
+      Publication,
+
+      Newspaper,
+
+      Video,
+
+      Sport
     }
   }
 }
--- Project/Models/Library.cs
+++ Project/Models/Library.cs
@@ -8,8 +8,9 @@ namespace console_library.Models
     public string Location { get; set; }
     public string Name { get; set; }
     private List<Book> Books { get; set; }
-
     private List<Book> CheckedOut { get; set; }
+    private List<Newspaper> Papers { get; set; }
+    private List<Magazine> Magazines { get; set; }
 
 
     public Library(string location, string name)
@@ -18,33 +19,72 @@ namespace console_library.Models
       Name = name;
       Books = new List<Book>();
       CheckedOut = new List<Book>();
+      Papers = new List<Newspaper>();
+      Magazines = new List<Magazine>();
+    }
+
+    public void PrintPublications()
+    {
+      System.Console.WriteLine(@"Would you like to stay and read a (N)ewspaper/Magazine
+             OR
+would you like to check out a (B)ook?");
     }
 
     public void PrintBooks()
     {
       System.Console.WriteLine("Please choose from the available books:");
+      System.Console.WriteLine("Select a book number to checkout (Q)uit, or (R)eturn a book");
       for (int i = 0; i < Books.Count; i++)
       {
         Book currentBook = Books[i];
         Console.WriteLine($"{i + 1} {currentBook.Title} - {currentBook.Author}");
       }
-      System.Console.WriteLine("Seleect a book number to checkout (Q)uit, or (R)eturn a book");
     }
 
     public void PrintCheckedOut()
     {
       System.Console.WriteLine("Books you have checked out.");
+      System.Console.WriteLine("Select a book number to return, (Q)uit, or (A)vailable to see available books");
       for (int i = 0; i < CheckedOut.Count; i++)
       {
         Book currentBook = CheckedOut[i];
         Console.WriteLine($"{i + 1} {currentBook.Title} - {currentBook.Author}");
       }
-      System.Console.WriteLine("Seleect a book number to return, (Q)uit, or (A)vailable to see available books");
+    }
+
+    public void PrintPapers()
+    {
+      System.Console.WriteLine("Newspapers:");
+      for (int i = 0; i < Papers.Count; i++)
+      {
+        System.Console.WriteLine("TEST");
+        Newspaper currentPaper = Papers[i];
+        Console.WriteLine($"{i + 1} {currentPaper.Title} - {currentPaper.PublicationDate}");
+      }
+      System.Console.WriteLine("");
+    }
+
+    public void PrintMagazines()
+    {
+      System.Console.WriteLine("Magazines:");
+      for (int i = 0; i < Magazines.Count; i++)
+      {
+        Magazine currentMagazine = Magazines[i];
+        Console.WriteLine($"{i + 1} {currentMagazine.Title} - {currentMagazine.PublicationDate}");
+      }
     }
 
     public void AddBook(Book book)
     {
       Books.Add(book);
+    }
+    public void AddPaper(Newspaper paper)
+    {
+      Papers.Add(paper);
+    }
+    public void AddMagazine(Magazine magazine)
+    {
+      Magazines.Add(magazine);
     }
 
 
